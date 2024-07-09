7e76860ba531e6ae9477941dcca54b81087a2755
oluwatobi <oluwatobiajayi001@gmail.com>
2024-07-09 13:43:13 +0100

    modified:   resources/views/customer/sub/about.blade.php

--- resources/views/customer/sub/about.blade.php
+++ resources/views/customer/sub/about.blade.php
@@ -1,10 +1,21 @@
-<h1>Customer About Us.</h1>
-<p>This is the customer sub about page.</p>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="font-bold">Customer About Us.</h1>
+    <p>This is the customer sub about page.</p>
 
-Country Name: {{ $country }}
-<br />
+    Country Name: {{ $country }}
+    <br />
 
-City Name: {{ $city }}
-<br />
+    City Name: {{ $city }}
+    <br />
 
-State Name: {{ $state }}
+    State Name: {{ $state }}
+</body>
+</html>
+
